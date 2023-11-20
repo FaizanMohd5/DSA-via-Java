bb5365d062b44e30dbdecc4e6f1c706378d6ca64
Faizan Mohammed <106433504+FaizanMohd5@users.noreply.github.com>
2023-11-20 23:51:53 +0530

    Create Sorting.java

---
+++ Sorting.java
@@ -0,0 +1,8 @@
+public interface Sorting {
+    void bubbleSort(); //by default public abstract.
+    void selectionSort();
+    void insertionSort();
+    void quickSortCode(int low, int high);
+
+    void mergeSortCode(int low, int high);
+}
