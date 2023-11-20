7d9996f74bb597b7fe9728fd7f7e2f8b9a3dfda5
Faizan Mohammed <106433504+FaizanMohd5@users.noreply.github.com>
2023-11-20 23:50:10 +0530

    Update DynamicArray.java

--- DynamicArray.java
+++ DynamicArray.java
@@ -1,46 +1,44 @@
-public class DynamicArray {
+public class DynamicArray implements Sorting {
     //1. DynamicArray class with lastIndex and ptr
-    private int lastIndex=-1;
-    private Object[] ptr;
+    private int lastIndex = -1;
+    private int[] ptr;
 
     //2. Constructor for taking size and allocating memory
-    public DynamicArray(int size){
-        ptr = new Object[size];
+    public DynamicArray(int size) {
+        ptr = new int[size];
     }
 
     //3. Method to double the size of array
-    private void doubleArray(){
+    private void doubleArray() {
         try {
-            Object[] temp = new Object[ptr.length*2];
+            int[] temp = new int[ptr.length * 2];
             System.arraycopy(ptr, 0, temp, 0, lastIndex + 1);
             ptr = temp;
-        }
-        catch (NullPointerException e){
+        } catch (NullPointerException e) {
             System.out.println("doubleArray: null pointer exception");
         }
     }
 
     //4. Method to half the size of an array
-    private void halfArray(){
-        try{
-            Object[] temp = new Object[ptr.length/2];
-            System.arraycopy(ptr,0,temp,0,lastIndex+1);
+    private void halfArray() {
+        try {
+            int[] temp = new int[ptr.length / 2];
+            System.arraycopy(ptr, 0, temp, 0, lastIndex + 1);
             ptr = temp;
-        }
-        catch (NullPointerException e){
+        } catch (NullPointerException e) {
             System.out.println("half array: null pointer exception");
         }
     }
 
     //5. Append method
-    public void appendElement(Object data){
-            if(lastIndex+1==ptr.length)
-                doubleArray();
-            ptr[++lastIndex]=data;
+    public void appendElement(int data) {
+        if (lastIndex + 1 == ptr.length)
+            doubleArray();
+        ptr[++lastIndex] = data;
     }
 
     //6. Insert data at given index method
-    public void insertElementAt(int index, Object data){
+    public void insertElementAt(int index, int data) {
         try {
             if (index < 0 || index > lastIndex + 1)
                 throw new ArrayIndexOutOfBoundsException("insertElementAt: invalid index");
@@ -51,46 +49,79 @@ public class DynamicArray {
             }
             ptr[index] = data;
             lastIndex++;
-        } catch(ArrayIndexOutOfBoundsException e){
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println(e.getMessage());
         }
     }
 
     //9. Edit element at a given index method
-    public void editElementAt(int index, Object data){
+    public void editElementAt(int index, int data) {
         try {
             if (index < 0 || index > lastIndex)
                 throw new ArrayIndexOutOfBoundsException("Invalid index");
             ptr[index] = data;
-        }
-        catch(ArrayIndexOutOfBoundsException e){
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println(e.getMessage());
         }
     }
 
     //8. Delete element at a given index method
-    public void deleteElementAt(int index){
+    public void deleteElementAt(int index) {
         try {
             if (index < 0 || index > lastIndex)
                 throw new ArrayIndexOutOfBoundsException("Invalid Index");
-            if (lastIndex + 1 == ptr.length / 2 && ptr.length>1)
+            if (lastIndex + 1 == ptr.length / 2 && ptr.length > 1)
                 halfArray();
             for (int i = index; i < lastIndex; i++)
                 ptr[i] = ptr[i + 1];
             lastIndex--;
-        }
-        catch(ArrayIndexOutOfBoundsException e){
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println(e.getMessage());
         }
     }
 
     //9. Total number of elements present method
-    public int getCount(){
-        return lastIndex+1;
+    public int getCount() {
+        return lastIndex + 1;
     }
 
     //10. Check if array is empty or not method
-    public boolean isArrayEmpty(){
-        return lastIndex==-1;
+    public boolean isArrayEmpty() {
+        return lastIndex == -1;
+    }
+
+    //11. Get value stored at a specific index
+    public int getValueAt(int index) {
+        return ptr[index];
+    }
+
+    //12. Get capacity of array
+    public int getCapacity() {
+        return ptr.length;
+    }
+
+    // display method
+    public void display() {
+        for (int i = 0; i <= lastIndex; i++) {
+            System.out.print(ptr[i] + " ");
+        }
+        System.out.println("Last Index: " + lastIndex + " length of array: " + getCapacity());
+    }
+
+    
+    // driver method
+    public static void driver() {
+        DynamicArray dynArray = new DynamicArray(4);
+
+        dynArray.appendElement(10);
+        dynArray.appendElement(9);
+        dynArray.appendElement(1);
+        dynArray.appendElement(2);
+        dynArray.appendElement(15);
+        dynArray.appendElement(16);
+        dynArray.appendElement(11);
+
+        dynArray.mergeSort();
+        dynArray.display();
     }
 }
